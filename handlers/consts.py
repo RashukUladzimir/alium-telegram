43df44dce7df6d0ca220fddd474fd21e4235f9bf
Uladzimir Rashuk <rashuk.vova@gmail.com>
2023-03-19 01:44:04 +0300

    changed messages

--- handlers/consts.py
+++ handlers/consts.py
@@ -1,24 +1,30 @@
-GET_USER_TASKS = 'Pick next task'
-WITHDRAWAL = 'Withdraw'
-CHANGE_WALLET_ADDRESS = 'Change Wallet address'
+GET_USER_TASKS = '📍Pick next task📍'
+WITHDRAWAL = '💸 Withdraw 💸'
+CHANGE_WALLET_ADDRESS = '🔐 Change Wallet Address 🔐'
 
 WELCOME_MESSAGE = '''
-Our Referral program is back - complete simple tasks, send proofs, invite friends and receive rewards: 
-- Up to 1,5 USDT for simple tasks 💰
-- 0,15 USDT for each referral 💸
+💸 Our Referral Program is back - complete simple tasks, invite your friends and receive rewards: 
 
-Once you achieve 10 USDT balance you'll be able to withdraw your reward
-Don't try to withdraw before you achieve 10 USDT limit, unless the priority of your next withdrawal will be downgraded.
+- Up to 1,5 USDT for simple tasks 📝
+- 0,15 USDT for each referral 👥
 
-Total prize pool for this event is $5000, hurry up and do your tasks so you can pick up your part!
+🔗Once you achieve 10 USDT balance you'll be able to withdraw your reward
+
+❗️Don't try to withdraw before you achieve 10 USDT limit, unless the priority of your next withdrawal will be downgraded!
+
+💰Total prize pool for the event is $5000, hurry up and do your tasks so you can pick up your part!
 '''
 
 WALLET_MESSAGE = '''
-Let me know your wallet address where you’d like to receive your rewards and which you use for swaps and connect to Alium.Finance platform: 
+🔗Send your ETH/BSC/Polygon wallet address where you’d like to receive your rewards and which you use for swaps and connect to Alium.Finance platform:
 '''
 
 DISCORD_MESSAGE = '''
-Alright, now provide me your Discord account👾 which is connected to Alium Discord server (“https://discord.gg/XpjHmmrn74")
+✅ Alright
+
+👾 Now provide your Discord account which is connected to Alium Discord server (“https://discord.gg/XpjHmmrn74")
+
+e.g. - Alium#1122
 '''
 
 MAIN_MENU_MESSAGE = '''
@@ -26,5 +32,5 @@ MAIN_MENU_MESSAGE = '''
 💰 Your current Balance: {} 
 💰 Your current Withdrawal Balance: {} 
 📢 Your referrals counter: {}
-📝 Your referral link: {}
+🔗 Your referral link: {}
 '''
